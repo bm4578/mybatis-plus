170636a5296cb4b8774fffccc1c9312ef3e30680
codetype <m15319631269@163.com>
2022-12-21 09:59:58 +0800

    修复IP地址显示异常

--- src/test/java/xyz/onlytype/mybatisplus/MybatisPlusApplicationTests.java
+++ src/test/java/xyz/onlytype/mybatisplus/MybatisPlusApplicationTests.java
@@ -3,11 +3,17 @@ package xyz.onlytype.mybatisplus;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
 @SpringBootTest
 class MybatisPlusApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
+
 
 }
